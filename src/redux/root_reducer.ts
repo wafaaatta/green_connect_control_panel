27347bbf18d28ef2e3c17635a47f08a3be21b636
wafaa <wafaaatta04@gmail.com>
2024-09-22 21:47:07 +0300

    créer un magasin pour la catégorie d'article et définir la méthode d'obtention des catégories

--- src/redux/root_reducer.ts
+++ src/redux/root_reducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import notification_store from './stores/notification_store';
+import article_category_store from './stores/article_category_store';
 
 
 const rootReducer = combineReducers({
     notification_store: notification_store,
+    article_category_store: article_category_store
 });
 
 export default rootReducer;
---
+++ src/redux/stores/article_category_store.ts
@@ -0,0 +1,46 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import ArticleCategoryState from "../../interfaces/states/ArticleCategoryState";
+import ApiError from "../../interfaces/ApiError";
+import { AxiosError } from "axios";
+import axiosHttp from "../../utils/axios_client";
+
+const initialState: ArticleCategoryState = {
+    categories: [],
+    status_code: null,
+    loading: false,
+    error: null
+}
+
+const getAllCategories = createAsyncThunk(
+    'articleCategory/getAllCategories',
+    async () => {
+        try{
+            const response = await axiosHttp.get('/article-categories')
+            return response.data
+        }catch(error){
+            throw ApiError.from(error as AxiosError)
+        }
+    }
+)
+
+const articleCategorySlice = createSlice({
+    name: 'articleCategory',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(getAllCategories.pending, (state) => {
+                state.loading = true
+            })
+            .addCase(getAllCategories.fulfilled, (state, action) => {
+                state.loading = false
+                state.categories = action.payload
+            })
+            .addCase(getAllCategories.rejected, (state, action) => {
+                state.loading = false
+                state.error = action.error.message || 'Something went wrong'
+            })
+    }
+})
+
+export default articleCategorySlice.reducer
